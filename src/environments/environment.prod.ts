39ebfc1151b753f651604d1d7dd927029a28a233
bshu <bshu@PF3M7F9K>
2022-11-14 11:33:30 +0700

    feat: config firebase service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'party2gether-b71a8',
+    appId: '1:381736140656:web:792d470d9b0e8577f119b5',
+    storageBucket: 'party2gether-b71a8.appspot.com',
+    apiKey: 'AIzaSyBBMCODcVHCNAlxCmF3WC9dFuCWyFo15Mc',
+    authDomain: 'party2gether-b71a8.firebaseapp.com',
+    messagingSenderId: '381736140656',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'party2gether-b71a8',
+    appId: '1:381736140656:web:792d470d9b0e8577f119b5',
+    storageBucket: 'party2gether-b71a8.appspot.com',
+    apiKey: 'AIzaSyBBMCODcVHCNAlxCmF3WC9dFuCWyFo15Mc',
+    authDomain: 'party2gether-b71a8.firebaseapp.com',
+    messagingSenderId: '381736140656',
+  },
   production: false
 };
 
